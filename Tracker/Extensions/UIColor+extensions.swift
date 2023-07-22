5efbf9f42d714c2900781a65c5c9c6ab78905a3a
Ubskiy <ArsenAvakov1@yandex.ru>
2023-07-23 00:05:21 +0300

    Убраны системные комментарии, и исправлен force unwrap в UIColor+extensions

--- Tracker/Extensions/UIColor+extensions.swift
+++ Tracker/Extensions/UIColor+extensions.swift
@@ -2,36 +2,36 @@ import UIKit
 
 extension UIColor {
     // Дефолтные цвета
-    static let ypBackgroundDay = UIColor(named: "YPBackgroundDay")!
-    static let ypBlackDay = UIColor(named: "YPBlackDay")!
-    static let ypWhiteDay = UIColor(named: "YPWhiteDay")!
+    static let ypBackgroundDay = UIColor(named: "YPBackgroundDay") ?? UIColor.white
+    static let ypBlackDay = UIColor(named: "YPBlackDay") ?? UIColor.black
+    static let ypWhiteDay = UIColor(named: "YPWhiteDay") ?? UIColor.white
     
-    static let ypBackgroundNight = UIColor(named: "YPBackgroundNight")!
-    static let ypBlackNight = UIColor(named: "YPBlackNight")!
-    static let ypWhiteNight = UIColor(named: "YPWhiteNight")!
+    static let ypBackgroundNight = UIColor(named: "YPBackgroundNight") ?? UIColor.white
+    static let ypBlackNight = UIColor(named: "YPBlackNight") ?? UIColor.black
+    static let ypWhiteNight = UIColor(named: "YPWhiteNight") ?? UIColor.white
     
-    static let ypGray = UIColor(named: "YPGray")!
-    static let ypLightGray = UIColor(named: "YPLightGray")!
-    static let ypRed = UIColor(named: "YPRed")!
-    static let ypBlue = UIColor(named: "YPBlue")!
+    static let ypGray = UIColor(named: "YPGray") ?? UIColor.gray
+    static let ypLightGray = UIColor(named: "YPLightGray") ?? UIColor.gray
+    static let ypRed = UIColor(named: "YPRed") ?? UIColor.red
+    static let ypBlue = UIColor(named: "YPBlue") ?? UIColor.blue
     
     // Цвета Селекций
-    static let ypSelection1 = UIColor(named: "YPSelection1")!
-    static let ypSelection2 = UIColor(named: "YPSelection2")!
-    static let ypSelection3 = UIColor(named: "YPSelection3")!
-    static let ypSelection4 = UIColor(named: "YPSelection4")!
-    static let ypSelection5 = UIColor(named: "YPSelection5")!
-    static let ypSelection6 = UIColor(named: "YPSelection6")!
-    static let ypSelection7 = UIColor(named: "YPSelection7")!
-    static let ypSelection8 = UIColor(named: "YPSelection8")!
-    static let ypSelection9 = UIColor(named: "YPSelection9")!
-    static let ypSelection10 = UIColor(named: "YPSelection10")!
-    static let ypSelection11 = UIColor(named: "YPSelection11")!
-    static let ypSelection12 = UIColor(named: "YPSelection12")!
-    static let ypSelection13 = UIColor(named: "YPSelection13")!
-    static let ypSelection14 = UIColor(named: "YPSelection14")!
-    static let ypSelection15 = UIColor(named: "YPSelection15")!
-    static let ypSelection16 = UIColor(named: "YPSelection16")!
-    static let ypSelection17 = UIColor(named: "YPSelection17")!
-    static let ypSelection18 = UIColor(named: "YPSelection18")!
+    static let ypSelection1 = UIColor(named: "YPSelection1") ?? UIColor.gray
+    static let ypSelection2 = UIColor(named: "YPSelection2") ?? UIColor.gray
+    static let ypSelection3 = UIColor(named: "YPSelection3") ?? UIColor.gray
+    static let ypSelection4 = UIColor(named: "YPSelection4") ?? UIColor.gray
+    static let ypSelection5 = UIColor(named: "YPSelection5") ?? UIColor.gray
+    static let ypSelection6 = UIColor(named: "YPSelection6") ?? UIColor.gray
+    static let ypSelection7 = UIColor(named: "YPSelection7") ?? UIColor.gray
+    static let ypSelection8 = UIColor(named: "YPSelection8") ?? UIColor.gray
+    static let ypSelection9 = UIColor(named: "YPSelection9") ?? UIColor.gray
+    static let ypSelection10 = UIColor(named: "YPSelection10") ?? UIColor.gray
+    static let ypSelection11 = UIColor(named: "YPSelection11") ?? UIColor.gray
+    static let ypSelection12 = UIColor(named: "YPSelection12") ?? UIColor.gray
+    static let ypSelection13 = UIColor(named: "YPSelection13") ?? UIColor.gray
+    static let ypSelection14 = UIColor(named: "YPSelection14") ?? UIColor.gray
+    static let ypSelection15 = UIColor(named: "YPSelection15") ?? UIColor.gray
+    static let ypSelection16 = UIColor(named: "YPSelection16") ?? UIColor.gray
+    static let ypSelection17 = UIColor(named: "YPSelection17") ?? UIColor.gray
+    static let ypSelection18 = UIColor(named: "YPSelection18") ?? UIColor.gray
 }
